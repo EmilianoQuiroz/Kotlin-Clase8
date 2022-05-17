80a3973b1e5bbc20d5232ea35588e017e12fb40e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-05-17 19:23:09 -0300

    Cambios guardados

--- src/Funciones.kt
+++ src/Funciones.kt
@@ -1,4 +1,13 @@
 fun main(){
 
-    
+    /*
+     *********************************
+     *          FUNCIONES            *
+     *********************************
+     */
+    //Con argumentos por defecto
+    bienvenida()//Si llamamos a la funcion sin pasarle argumentos, tomara el que tiene por defecto
+}
+fun bienvenida(nombre:String = "Maria"){//En este caso el tipo de dato de retorno es Unit (Puede omitirse)
+    println("Bienvenido/a usuario $nombre")
 }
